4ce68f3d02bfc197f031826ab499cce9dff3072f
BAHIRU YIMOLAL <88880193+Bahiru-Yimolal@users.noreply.github.com>
2024-05-08 15:38:23 +0300

    Update frontend.js

--- routes/frontend.js
+++ routes/frontend.js
@@ -1,1 +1,13 @@
 
+const express = require("express");
+const { protect } = require("../middlewares/authMiddleware");
+const passport = require("passport");
+require("../controllers/googleAuth");
+
+const { authGoogleUser } = require("../controllers/userControllers");
+
+const router = express.Router();
+
+router.route("/login-success").get(authGoogleUser);
+
+module.exports = router;
